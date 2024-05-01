c8b020562a58f03e38a4a31f3a9be0a3fded46d8
Niranjan Kumar K S <niranjankumarks@outlook.com>
2024-05-01 13:39:19 +0530

    next

--- index.js
+++ index.js
@@ -26,6 +26,40 @@ function generateDirectoryTree(currentPath, parentNode = null) {
   return tree;
 }
 
+function checkDirectoryStructure(directoryTree) {
+  const baseSet = new Set(directoryTree.map(_ => _.name));
+  const baseSetTypes = new Set(directoryTree.map(_ => _.type))
+  if (
+    baseSet.size === 2 &&
+    baseSetTypes.size === 1 &&
+    baseSet.has("java") &&
+    baseSet.has("resources") &&
+    baseSetTypes.has("directory")
+  ) {
+    directoryTree.forEach(dtree => {
+      const { name, files } = dtree;
+      if (
+        files.length === 1 && files[0].name === "in" && files[0].type === "directory" &&
+        files[0].files.length === 1 && files[0].files[0].name === "hingua" && files[0].files[0].type === "directory" &&
+        files[0].files[0].files.length === 1 && files[0].files[0].files[0].name === "samplecodepackage" && files[0].files[0].files[0].type === "directory"
+      ) {} 
+      else {
+        console.log(directoryTree);
+        throw new Error(`The src/main/${name} can contain only classpath in.hingua.samplecode`);
+      }
+    })
+  } else {
+    console.log(directoryTree);
+    throw new Error("The src/main can onltain only two directories named resources and java.");
+  }
+}
+
 const currentDirectory = 'src/main';
 const directoryTree = generateDirectoryTree(currentDirectory);
-console.log(JSON.stringify(directoryTree, null, 2));
+checkDirectoryStructure(directoryTree)
+
+
+
+
+
+// console.log(JSON.stringify(directoryTree, null, 2));
